31d4cbf1aca241708272a78dffad165f095b0333
Armani666 <abiram_sanser@hotmail.com>
2026-02-27 01:01:27 -0600

    update site + security + vercel workflow 2

---
+++ config.js
@@ -0,0 +1,5 @@
+window.RGL_CONFIG = {
+  supabaseUrl: "https://drjwafzfiyprmcaqqeww.supabase.co",
+  supabasePublishableKey: "sb_publishable_wtRKVp8sLN1iZ4FlNyXKHQ_Nos6sD0-",
+  catalogView: "catalog_products"
+};
